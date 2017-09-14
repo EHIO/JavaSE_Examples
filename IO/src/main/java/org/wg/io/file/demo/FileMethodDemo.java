e991fba4dff9bafc2c742786e595a4dfd3b12e6a
EHIO <1289868863@qq.com>
2017-09-15 01:00:27 +0800

    整理文件操作代码

--- IO/src/main/java/org/wg/io/file/demo/FileMethodDemo.java
+++ IO/src/main/java/org/wg/io/file/demo/FileMethodDemo.java
@@ -1,5 +1,7 @@
 package org.wg.io.file.demo;
 
+import org.junit.Test;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -18,32 +20,9 @@ public class FileMethodDemo {
      * <p>
      * 注意：你到底要创建文件还是文件夹，你最清楚，方法不要调错了。
      */
-
+    @Test
     public void create() throws IOException {
-        // 需求：我要在e盘目录下创建一个文件夹demo
-//		File file = new File("f:\\demo");
-//		System.out.println("mkdir:" + file.mkdir());
-
-        // 需求:我要在e盘目录demo下创建一个文件a.txt
-//		File file2 = new File("f:\\demo\\newfile.txt");
-//		System.out.println("createNewFile:" + file2.createNewFile());
-
-        // 需求：我要在e盘目录test下创建一个文件b.txt
-        // Exception in thread "main" java.io.IOException: 系统找不到指定的路径。
-        // 注意：要想在某个目录下创建内容，该目录首先必须存在。
-        // File file3 = new File("e:\\test\\b.txt");
-        // System.out.println("createNewFile:" + file3.createNewFile());
-
-        // 需求:我要在e盘目录test下创建aaa目录
-//		 File file4 = new File("f:\\test\\aaa");
-//		 System.out.println("mkdir:" + file4.mkdir());
-
-        // 其实我们有更简单的方法
-//		File file7 = new File("e:\\aaa\\bbb\\ccc\\ddd");
-//		System.out.println("mkdirs:" + file7.mkdirs());
-
-        // 看下面的这个东西：
-        File file8 = new File("f:\\temp\\a.txt");
+        File file8 = new File("e:\\temp\\a.txt");
         System.out.println("mkdirs:" + file8.mkdirs());
     }
 
@@ -57,32 +36,13 @@ public class FileMethodDemo {
      *
      * @throws IOException
      */
-
+    @Test
     public void delete() throws IOException {
-        // 我不小心写成这个样子了
         File file = new File("a.txt");
         System.out.println("createNewFile:" + file.createNewFile());
-
-        // 继续玩几个
-        File file2 = new File("aaa\\bbb\\ccc");
-        System.out.println("mkdirs:" + file2.mkdirs());
-
         // 删除功能：我要删除a.txt这个文件
         File file3 = new File("a.txt");
         System.out.println("delete:" + file3.delete());
-
-        // 删除功能：我要删除ccc这个文件夹
-        File file4 = new File("aaa\\bbb\\ccc");
-        System.out.println("delete:" + file4.delete());
-
-        // 删除功能：我要删除aaa文件夹
-        // File file5 = new File("aaa");
-        // System.out.println("delete:" + file5.delete());
-
-        File file6 = new File("aaa\\bbb");
-        File file7 = new File("aaa");
-        System.out.println("delete:" + file6.delete());
-        System.out.println("delete:" + file7.delete());
     }
 
     /**
@@ -91,15 +51,10 @@ public class FileMethodDemo {
      * <p>
      * 路径以盘符开始：绝对路径 c:\\a.txt 路径不以盘符开始：相对路径 a.txt
      */
-
+    @Test
     public void rename() {
-        // 创建一个文件对象
-        // File file = new File("林青霞.jpg");
-        // // 需求：我要修改这个文件的名称为"东方不败.jpg"
-        // File newFile = new File("东方不败.jpg");
-        // System.out.println("renameTo:" + file.renameTo(newFile));
-
-        File file2 = new File("东方不败.jpg");
+        File file2 = new File("e:\\东方不败.jpg");
+        file2.mkdirs();
         File newFile2 = new File("e:\\林青霞.jpg");
         System.out.println("renameTo:" + file2.renameTo(newFile2));
     }
