9341c0c0d09ff3dccd8a6f25d7637dd2d944f240
EHIO <1289868863@qq.com>
2017-12-08 00:24:11 +0800

    设计模式迭代-代理

--- Design Pattern/src/structural/proxy/javaproxy/example1/ProxyTest.java
+++ Design Pattern/src/structural/proxy/javaproxy/example1/ProxyTest.java
@@ -13,11 +13,14 @@ public class ProxyTest {
         // 得到代理对象..注意这里的第一个参数 要和userDao是同一个类加载器
         // 第二个参数是实现哪个接口，要和被代理实现同样的接口
         // 第三个参数是织入的类，该类实现了InvocationHandle接口
-        UserDao userDaoProxy = (UserDao) Proxy.newProxyInstance(userDao.getClass().getClassLoader(),
-                new Class[]{UserDao.class}, li);
+        UserDao userDaoProxy = (UserDao) Proxy.newProxyInstance(
+                userDao.getClass().getClassLoader(),
+                new Class[]{UserDao.class},
+                li);
         userDaoProxy.save(new User());
 
         // 代理类的名字
         System.out.println(userDaoProxy.getClass().getName());
     }
+
 }
