1016fa5169cc1850f7f28de94e1603aa0602244c
EHIO <1289868863@qq.com>
2017-12-15 14:48:10 +0800

    设计模式迭代-装饰者

--- Design Pattern/src/structural/decorator/io/LowerCaseInputStream.java
+++ Design Pattern/src/structural/decorator/io/LowerCaseInputStream.java
@@ -10,6 +10,7 @@ public class LowerCaseInputStream extends FilterInputStream {
         super(in);
     }
 
+    @Override
     public int read() throws IOException {
         int c = super.read();
         return (c == -1 ? c : Character.toLowerCase((char) c));
