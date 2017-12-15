a3c066c53051dd0dea09d8621a9e18132d2585b3
EHIO <1289868863@qq.com>
2017-12-15 14:11:53 +0800

    设计模式迭代-装饰者

--- Design Pattern/src/structural/decorator/starbuzz/StarbuzzCoffee.java
+++ Design Pattern/src/structural/decorator/starbuzz/StarbuzzCoffee.java
@@ -9,19 +9,19 @@ public class StarbuzzCoffee {
     public static void main(String[] args) {
         // 浓缩咖啡
         Beverage beverage = new Espresso();
-        System.err.println(beverage.getDescription() + ",$" + beverage.cost());
+        System.err.println(beverage.getDescription() + ", 价钱：$" + beverage.cost());
         // 深焙咖啡
         Beverage beverage2 = new DarkRoast();
         // 两份摩卡和奶泡装饰深焙咖啡
         beverage2 = new Mocha(beverage2);
         beverage2 = new Mocha(beverage2);
         beverage2 = new Whip(beverage2);
-        System.err.println(beverage2.getDescription() + ",$" + beverage2.cost());
+        System.err.println(beverage2.getDescription() + ", 价钱：$" + beverage2.cost());
 
         Beverage beverage3 = new HouseBlend();
         beverage3 = new Mocha(beverage3);
         beverage3 = new Soy(beverage3);
         beverage3 = new Whip(beverage3);
-        System.err.println(beverage3.getDescription() + ",$" + beverage3.cost());
+        System.err.println(beverage3.getDescription() + ", 价钱：$" + beverage3.cost());
     }
 }
