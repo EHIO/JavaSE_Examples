87a08bd454c9a34e2bb7baa18a38cdc0fbb58584
EHIO <1289868863@qq.com>
2018-01-17 17:19:17 +0800

    IO 例子

--- IO/src/main/java/org/wg/io/bytestream/CopyTest.java
+++ IO/src/main/java/org/wg/io/bytestream/CopyTest.java
@@ -8,8 +8,8 @@ public class CopyTest {
 
     @Test
     public void copy_4() throws IOException {
-        FileInputStream fis = new FileInputStream("fis.txt");
-        FileOutputStream fos = new FileOutputStream("fos.txt");
+        FileInputStream fis = new FileInputStream("src/main/resources/fis.txt");
+        FileOutputStream fos = new FileOutputStream("src/main/resources/fox.txt");
 
         int ch = 0;
 
@@ -27,8 +27,8 @@ public class CopyTest {
      */
     @Test
     public void copy_3() throws IOException {
-        FileInputStream fis = new FileInputStream("fis.txt");
-        FileOutputStream fos = new FileOutputStream("fos.txt");
+        FileInputStream fis = new FileInputStream("src/main/resources/fis.txt");
+        FileOutputStream fos = new FileOutputStream("src/main/resources/fox.txt");
 
         byte[] buf = new byte[fis.available()];
         fis.read(buf);
@@ -41,10 +41,10 @@ public class CopyTest {
     @Test
     public void copy_2() throws IOException {
 
-        FileInputStream fis = new FileInputStream("fis.txt");
+        FileInputStream fis = new FileInputStream("src/main/resources/fis.txt");
         BufferedInputStream bufis = new BufferedInputStream(fis);
 
-        FileOutputStream fos = new FileOutputStream("fos.txt");
+        FileOutputStream fos = new FileOutputStream("src/main/resources/fox.txt");
         BufferedOutputStream bufos = new BufferedOutputStream(fos);
 
         int ch = 0;
@@ -59,18 +59,13 @@ public class CopyTest {
 
     @Test
     public void copy_1() throws IOException {
-
-        FileInputStream fis = new FileInputStream("fis.txt");
-        FileOutputStream fos = new FileOutputStream("fos.txt");
-
+        FileInputStream fis = new FileInputStream("src/main/resources/fis.txt");
+        FileOutputStream fos = new FileOutputStream("src/main/resources/fox.txt");
         byte[] buf = new byte[1024];
-
-        int len = 0;
-
+        int len;
         while ((len = fis.read(buf)) != -1) {
             fos.write(buf, 0, len);
         }
-
         fos.close();
         fis.close();
     }
@@ -78,19 +73,18 @@ public class CopyTest {
 
     @Test
     public void method5() throws IOException {
-        FileInputStream fis = new FileInputStream("fis.txt");
+        FileInputStream fis = new FileInputStream("src/main/resources/fis.txt");
         BufferedInputStream bufis = new BufferedInputStream(fis);
 
-        FileOutputStream fos = new FileOutputStream("fox.txt");
+        FileOutputStream fos = new FileOutputStream("src/main/resources/fox.txt");
         BufferedOutputStream bufos = new BufferedOutputStream(fos);
 
         byte[] bys = new byte[1024];
-        int len = 0;
+        int len;
         while ((len = bufis.read(bys)) != -1) {
             bufos.write(bys, 0, len);
         }
-
-        bufis.close();
+        bufos.close();
         bufis.close();
     }
 }
