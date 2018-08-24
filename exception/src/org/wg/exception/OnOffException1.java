f74688e85077908f08641dfd5e0f3f7f0914b73c
EHIO <1160061218@qq.com>
2018-08-24 10:49:47 +0800

    java 异常例子

---
+++ exception/src/org/wg/exception/OnOffException1.java
@@ -0,0 +1,5 @@
+package org.wg.exception;
+
+//: exceptions/OnOffException1.java
+public class OnOffException1 extends Exception {
+} ///:~
---
+++ exception/src/org/wg/exception/OnOffException2.java
@@ -0,0 +1,5 @@
+package org.wg.exception;
+
+//: exceptions/OnOffException2.java
+public class OnOffException2 extends Exception {
+} ///:~
