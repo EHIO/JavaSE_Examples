4bd52cb95f79a099289084e77329eb79674036a3
Run <1160061218@qq.com>
2018-12-20 00:20:52 +0800

    并发

---
+++ Thread/src/org/wg/concurrency/daemon/DaemonsDontRunFinally.java
@@ -0,0 +1,32 @@
+package org.wg.concurrency.daemon;//: concurrency/DaemonsDontRunFinally.java
+
+import java.util.concurrent.TimeUnit;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * Daemon threads don't run the finally clause
+ * 守护程序线程不运行finally子句
+ */
+class ADaemon implements Runnable {
+    public void run() {
+        try {
+            print("Starting ADaemon");
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            print("Exiting via InterruptedException");
+        } finally {
+            print("This should always run?");
+        }
+    }
+}
+
+public class DaemonsDontRunFinally {
+    public static void main(String[] args) throws Exception {
+        Thread t = new Thread(new ADaemon());
+        t.setDaemon(true);
+        t.start();
+    }
+} /* Output:
+Starting ADaemon
+*///:~
