01559b2c86166e6f7db327cef805aa8854e55811
Run <1160061218@qq.com>
2019-04-20 18:33:59 +0800

    clear up

---
+++ annotations/src/Test.java
@@ -0,0 +1,12 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 标记注解，没有元素的注解
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Test {
+}
