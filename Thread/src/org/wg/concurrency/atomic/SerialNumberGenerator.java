f145f8d499623bff7d8b9a172af261bad3e9a846
Run <1160061218@qq.com>
2019-04-20 19:16:39 +0800

    clear up

--- Thread/src/org/wg/concurrency/atomic/SerialNumberGenerator.java
+++
@@ -1,9 +0,0 @@
-package org.wg.concurrency.atomic;//: concurrency/SerialNumberGenerator.java
-
-public class SerialNumberGenerator {
-    private static volatile int serialNumber = 0;
-
-    public static int nextSerialNumber() {
-        return serialNumber++; // Not thread-safe
-    }
-} ///:~
